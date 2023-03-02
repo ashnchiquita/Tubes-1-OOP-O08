f2edd527547c9227e76ecec7d924e8a45f1032ae
Rava Maulana <ravamaulana14@gmail.com>
2023-03-02 13:17:15 +0700

    Merge

--- src/valuables/combo.hpp
+++ src/valuables/combo.hpp
@@ -3,25 +3,27 @@
 
 #include <iostream>
 #include <vector>
+
 #include "card.hpp"
 #include "valuable.hpp"
 
 using namespace std;
 
 class Combo : public Valuable {
-    protected:
-        vector<Card> card;
-    public: 
-        float Value();
+ protected:
+  Card* cardList;
 
-        float pair();
-        float twoPair();
-        float threeOfAKind();
-        float straight();
-        float flush();
-        float fullHouse();
-        float fourOfAKind();
-        float straightFlush();
+ public:
+  float value();
+
+  float pair();
+  float twoPair();
+  float threeOfAKind();
+  float straight();
+  float flush();
+  float fullHouse();
+  float fourOfAKind();
+  float straightFlush();
 };
 
 #endif
